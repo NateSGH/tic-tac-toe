eedcdd510aa8a6155becdde80ae95d5f1976a6b8
Nate <for.education.n@yandex.ru>
2021-06-14 20:32:56 +0200

    Basic functionality done

--- script.js
+++ script.js
@@ -11,69 +11,122 @@ const gameBoard = (() => {
 })();
 
 const game = (() => {
+  const gameboardCont = document.querySelector(".gameboard-container");
+  const gameResContainer = document.querySelector(".game-result-container");
+  const gameResText = document.querySelector(".game-result-container");
+
   const checkPlayerWin = (player) => {
-    let gameInfo = {
-      result: "",
-      name: "",
-      mark: "",
-    };
-    for (let i = 0; i < 3; i++) {
+    const checkIfWin = () => {
+      for (let i = 0; i < 3; i++) {
+        if (
+          (gameBoard.array[i] === player.mark &&
+            gameBoard.array[i + 3] === player.mark &&
+            gameBoard.array[i + 6] === player.mark) || //columns
+          (gameBoard.array[i * 3] === player.mark &&
+            gameBoard.array[i * 3 + 1] === player.mark &&
+            gameBoard.array[i * 3 + 2] === player.mark) // rows
+        ) {
+          console.log(`${player.name} (${player.mark}) wins!`);
+
+          return {
+            result: "win",
+            name: player.name,
+            mark: player.mark,
+          };
+        }
+      }
+      // diagonal
       if (
-        (gameBoard.array[i] === player.mark &&
-          gameBoard.array[i + 3] === player.mark &&
-          gameBoard.array[i + 6] === player.mark) || //columns
-        (gameBoard.array[i * 3] === player.mark &&
-          gameBoard.array[i * 3 + 1] === player.mark &&
-          gameBoard.array[i * 3 + 2] === player.mark) // rows
+        (gameBoard.array[0] === player.mark &&
+          gameBoard.array[4] === player.mark &&
+          gameBoard.array[8] === player.mark) ||
+        (gameBoard.array[2] === player.mark &&
+          gameBoard.array[4] === player.mark &&
+          gameBoard.array[6] === player.mark)
       ) {
         console.log(`${player.name} (${player.mark}) wins!`);
-
         return {
           result: "win",
           name: player.name,
           mark: player.mark,
         };
       }
+    };
+
+    const checkIfTie = () => {
+      let defFlag = true;
+
+      for (let i = 0; i < 9; i++) {
+        defFlag = defFlag && gameBoard.array[i] !== undefined;
+      }
+
+      if (defFlag) {
+        console.log("It's a tie!");
+        return { result: "tie" };
+      }
+    };
+
+    let result = checkIfWin();
+    if (result === undefined) result = checkIfTie();
+    if (result !== undefined) {
+      return result;
     }
-    // diagonal
-    if (
-      (gameBoard.array[0] === player.mark &&
-        gameBoard.array[4] === player.mark &&
-        gameBoard.array[8] === player.mark) ||
-      (gameBoard.array[2] === player.mark &&
-        gameBoard.array[4] === player.mark &&
-        gameBoard.array[6] === player.mark)
-    ) {
-      console.log(`${player.name} (${player.mark}) wins!`);
-      return {
-        result: "win",
-        name: player.name,
-        mark: player.mark,
-      };
-    }
-    // TODO: tie
-    // if () {
-    //   console.log("It's a tie!");
-    //   return { result: "tie" };
-    // }
 
     console.log("test return");
     return { result: "none" };
   };
+
   const checkWin = (player1, player2) => {
     let resultX = checkPlayerWin(player1);
     let resultO = checkPlayerWin(player2);
     if (resultX.result === "win") {
-      alert(`${resultX.name} (${resultX.mark}) wins!`);
+      gameResText.textContent = `${resultX.name} (${resultX.mark}) wins!`;
+      displayResult();
+      return resultX.result;
     } else if (resultO.result === "win") {
-      alert(`${resultO.name} (${resultO.mark}) wins!`);
+      gameResText.textContent = `${resultO.name} (${resultO.mark}) wins!`;
+      displayResult();
+      return resultO.result;
     } else if (resultX.result === "tie" || resultO.result === "tie") {
-      alert("It's a tie!");
+      gameResText.textContent = "It's a tie!";
+      displayResult();
+      return "tie";
     }
   };
-  const playGame = () => {};
+
+  let clicks = 0;
+
+  const playGame = (player1, player2) => {
+    player1.placeMark();
+    player2.placeMark();
+
+    let listener = (e) => {
+      let gameRes = game.checkWin(player1, player2);
+      if (gameRes === "win" || gameRes === "tie") {
+        return;
+      } else if (clicks < 5) {
+        clicks++;
+        console.log(clicks);
+        player1.placeMark();
+        player2.placeMark();
+        console.log("play game test");
+
+        // game.checkWin(player1, player2);
+      }
+    };
+    gameboardCont.addEventListener("click", listener);
+  };
+
+  const displayResult = () => {
+    gameResContainer.style.display = "block";
+  };
+
+  const hideResult = () => {
+    gameResContainer.style.display = "none";
+  };
+
   const restartGame = () => {};
-  return { checkWin };
+  return { checkWin, playGame };
 })();
 
 // Factories
@@ -100,13 +153,4 @@ const player = (name, mark) => {
 const player1X = player("Jeff", "X");
 const player2O = player("Alex", "O");
 
-player1X.placeMark();
-player2O.placeMark();
-player1X.placeMark();
-player2O.placeMark();
-player1X.placeMark();
-player2O.placeMark();
-player1X.placeMark();
-player2O.placeMark();
-player1X.placeMark();
-player2O.placeMark();
+game.playGame(player1X, player2O);
